9433fe45d13fac4360ae5fd7d07812e7c8a2ef02
javeria-zohaib <161313643+javeria-zohaib@users.noreply.github.com>
2024-12-08 23:18:00 +0500

    hackathon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -201,7 +201,7 @@ using modern web technologies.
       </div>
      
     </div>
-    <button className=" border-b-[0.5px] mx-auto mt-7 w-full text-[#4E4D93] font-medium ">View collection</button>
+    <button className=" border-b-[0.5px] mx-auto mt-4 w-full text-[#4E4D93] font-medium ">View collection</button>
   </div>
   
 </section>
@@ -231,6 +231,36 @@ using modern web technologies.
     </div>
   
 </section>
+<section className="text-gray-600 body-font">
+  <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+    <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-[#2A254B]">
+      From a studio in London to a global brand with
+      over 400 outlets
+      </h1>
+      <p className="mb-8 leading-relaxed text-[#505977]">
+      When we started Avion, the idea was simple. Make high quality furniture affordable and available for the mass market.
+      </p>
+      <p className="mb-48 leading-relaxed text-[#505977] ">
+      Handmade, and lovingly crafted furniture and homeware is what we live, breathe and design so our Chelsea boutique become the hotbed for the London interior design community.
+      </p>
+      <div className="flex justify-center">
+        <button className="inline-flex text-[#505977] bg-white border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+          Get in touch
+        </button>
+       
+      </div>
+    </div>
+    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 h-[603px]">
+      <img
+        className="object-cover object-center rounded h-full"
+        alt="hero"
+        src="/image.png"
+      />
+    </div>
+  </div>
+</section>
+
 
 
 
